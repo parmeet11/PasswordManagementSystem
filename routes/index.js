fa6cb1244012098bdee06f1b2663786d31f49c7f
Parmeet <parmeetsanotra@gmail.com>
2021-07-13 01:30:26 +0530

    frontend done 2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Password Management System' });
 });
 
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Password Management System' });
+});
+
 router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Password Management System' });
+});
+
+router.get('/passwordCategory', function(req, res, next) {
+  res.render('pwd_category', { title: 'Password Management System' });
 });
 
 module.exports = router;
