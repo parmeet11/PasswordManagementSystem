2b23e8a5bef8bdcc0f31450c20c43122ac3bbebc
Parmeet <parmeetsanotra@gmail.com>
2021-07-14 20:19:02 +0530

    connection done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+var userModule = require('../modules/user');
+var bodyParser = require("body-parser");
+
+//router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:true}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +16,26 @@ router.get('/login', function(req, res) {
 });
 
 router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'Password Management System' });
+  res.render('signup', { title: 'Password Management System', msg:''});
+});
+
+router.post('/signup', function(req, res, next) {
+  //var details = new userModule({
+   var username = req.body.username;
+   var email = req.body.email;
+   var password = req.body.password;
+   var cpassword = req.body.cpassword;
+ 
+   var details = new userModule({
+    "username": username,
+    "email": email,
+    "password": password
+  });
+  details.save((err,doc)=>{
+    if(err)  console.log(err);
+    res.render('signup', { title: 'Password Management System', msg:'user registered successfully' });
+  })
+  //console.log(details);
 });
 
 router.get('/passwordCategory', function(req, res, next) {
